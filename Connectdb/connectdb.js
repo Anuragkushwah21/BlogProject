fbae0ba624ba199a561b33e0e2121bdc26409b88
Anuragkushwah21 <Anuragkushwah5100@gmail.com>
2024-06-19 19:08:27 +0530

    asdfgh

--- Connectdb/connectdb.js
+++ Connectdb/connectdb.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-
+const Live_URL =
+"mongodb+srv://anuragkofficial21:ram123@cluster0.r3sc4oj.mongodb.net/blogproject?retryWrites=true&w=majority";
+// const Local_URL=("mongodb://0.0.0.0:27017/Blog")
 const connectDb = () => {
-    return mongoose
-        .connect("mongodb://0.0.0.0:27017/Blog")
-
+    mongoose
+      // .connect(Local_URL)
+      .connect(Live_URL)
+       
         .then(() => {
             console.log("connected sucessfully");
         })
